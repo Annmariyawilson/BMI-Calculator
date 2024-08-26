36aa369acba2958002533ad1498c089295c926d6
Akshay Wilson <Annmariyawilson>
2024-08-26 11:54:48 +0530

    bmi-calculator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import BMICalculator from './BMICalculator';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BMICalculator />
+    </div>
+  );
+};
+
+export default App;
